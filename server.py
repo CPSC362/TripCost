ef2c48d7686edbff91cf68f3e45757088a47b338
Austin White <austingym@gmail.com>
2014-02-19 14:12:48 -0800

    Added basic template
    To run on local system:
    $ source venv/bin/activate
    (venv) $ pip install -r requirements.txt
    (venv) $ foreman start

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+
+# from flask_wtf import Form, RecaptchaField
+# from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+#     BooleanField, SubmitField, IntegerField, FormField, validators
+# from wtforms.validators import Required
+
+#initialization
+
+configFile = None
+
+app = Flask(__name__, static_folder='assets')
+AppConfig(app, configFile)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+	
+Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', debug=True, port=port)
