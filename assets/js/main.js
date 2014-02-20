70c8239469111c3841da220a43678711f629f7bf
Austin White <austingym@gmail.com>
2014-02-20 01:26:09 -0800

    Added an about us page with gravatar support Separated out navbar into separate template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,27 @@
-$(document).ready(function() {
+$(function() {
 
-    var map;
-    function initialize() {
-        map = new google.maps.Map(document.getElementById('map-canvas'), {
-            zoom: 15,
-            center: new google.maps.LatLng(33.8596069, -117.8867514),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+    if (typeof google !== 'undefined') {
+        var map;
+        function initialize() {
+            map = new google.maps.Map(document.getElementById('map-canvas'), {
+                zoom: 15,
+                center: new google.maps.LatLng(33.8596069, -117.8867514),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+
+        $('.form-container .close').click(function(e) {
+            e.preventDefault();
+
+            $('.form-container').slideUp();
+        });
+
+        $('a[href="/#directions"]').click(function(e) {
+            e.preventDefault();
+
+            $('.form-container').slideDown();
         });
     }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
 });
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
+from flaskext.gravatar import Gravatar
 
 # from flask_wtf import Form, RecaptchaField
 # from wtforms import TextField, HiddenField, ValidationError, RadioField,\
@@ -14,12 +17,25 @@ configFile = None
 app = Flask(__name__, static_folder='assets')
 AppConfig(app, configFile)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+# Gravatar initialization
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    use_ssl=False,
+                    base_url=None)
 	
 Bootstrap(app)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
