71292bf1d6fee93f9e809ea214514e413c4a73ef
Austin White <austingym@gmail.com>
2014-03-27 15:03:51 -0700

    Formatted the costs to USD currency format

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,16 @@ $.fn.serializeObject = function() {
     return e
 }
 
+Handlebars.registerHelper("formatMoney", function(number) {
+    var decimalPlaces = isNaN(decimalPlaces = Math.abs(decimalPlaces)) ? 2 : decimalPlaces,
+        decimalSeparator = decimalSeparator == undefined ? "." : decimalSeparator,
+        commaSeparator = commaSeparator == undefined ? "," : commaSeparator,
+        sign = number < 0 ? "-" : "",
+        i = parseInt(number = Math.abs(+number || 0).toFixed(decimalPlaces)) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return sign + (j ? i.substr(0, j) + commaSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + commaSeparator) + (decimalPlaces ? decimalSeparator + Math.abs(number - i).toFixed(decimalPlaces).slice(2) : "");
+});
+
 $(function() {
     $('.dropdown-menu form').click(function(e) {
         e.stopPropagation();
