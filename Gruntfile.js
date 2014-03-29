ae34d7fb86331aea21b298f4e272c7c0abf6f832
Austin White <austingym@gmail.com>
2014-03-29 00:59:48 -0700

    Added Grunt.js (http://gruntjs.com/). From the Grunt.js website:
    Why use a task runner?
    In one word: automation. The less work you have to do when performing repetitive tasks like minification, compilation, unit testing, linting, etc, the easier your job becomes. After you've configured it, a task runner can do most of that mundane work for you—and your team—with basically zero effort.
    
    Basically, what I've set it up to do is combine all of the js and css files into one combined file for each. This way, the browser only needs to make 2 requests (in theory) for all of the necessary js and css. However, there are exceptions such as loading Google's Maps API from their servers.
    
    Grunt.js installation (assuming npm is installed: https://gist.github.com/isaacs/579814#file-node-and-npm-in-30-seconds-sh):
    $ npm install -g grunt-cli # install grunt globally
    $ npm install
    $ grunt

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            css: {
+                src: [
+                    'assets/css/vendor/bootstrap.css',
+                    'assets/css/vendor/bootstrap-theme.css',
+                    'assets/css/style.css'
+                ],
+                dest: 'assets/css/combined.css'
+            },
+
+            js: {
+                src: [
+                    'assets/js/vendor/handlebars-v1.3.0.js',
+                    'assets/js/vendor/v3_epoly.js',
+                    'assets/js/marker-generator.js',
+                    'assets/js/vendor/EdmundsAPI-sdk-javascript/edmunds.api.sdk.js',
+                    'assets/js/trip-cost.js',
+                    'assets/js/vehicle.js',
+                    'assets/js/fuel-economy.js',
+                    'assets/js/persistence.js',
+                    'assets/js/main.js'
+                ],
+                dest: 'assets/js/dist/combined.js'
+            }
+        },
+
+        cssmin: {
+            css: {
+                src: 'assets/css/combined.css',
+                dest: 'assets/css/combined.min.css'
+            }
+        },
+
+        uglify: {
+            js: {
+                files: {
+                    'assets/js/dist/combined.min.js': ['assets/js/dist/combined.js']
+                }
+            }
+        },
+
+        watch: {
+            files: ['assets/*'],
+            tasks: [
+                'concat:css',
+                'cssmin:css',
+                'concat:js',
+                'uglify:js'
+            ]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', [
+        'concat:css',
+        'cssmin:css',
+        'concat:js',
+        'uglify:js'
+    ]);
+};
