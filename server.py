c353f7f9c4b5c987bca5ebcb1e4b872212ed950f
Austin White <austingym@gmail.com>
2014-03-29 01:44:21 -0700

    Removed python code that is no longer used. Was able to handle all functionality within Javascript after switching to Edmunds.com API.

--- server.py
+++ server.py
@@ -15,8 +15,6 @@ import requests
 
 #initialization
 
-METERS_TO_MILES = 0.000621371
-
 configFile = None
 
 app = Flask(__name__, static_folder='assets')
@@ -42,100 +40,6 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/vehicle-menu-year')
-def vehicle_menu():
-    req = requests.get('http://www.fueleconomy.gov/ws/rest/vehicle/menu/year', headers={'Accept': 'application/json'})
-    return jsonify(req.json()), req.status_code
-
-@app.route('/vehicle-menu-make')
-def vehicle_make():
-    vehicle = {
-        'year': request.args.get('year')
-    }
-
-    req = requests.get('http://www.fueleconomy.gov/ws/rest/vehicle/menu/make', params=vehicle, headers={'Accept': 'application/json'})
-    return jsonify(req.json()), req.status_code
-
-@app.route('/vehicle-menu-model')
-def vehicle_model():
-    vehicle = {
-        'year': request.args.get('year'),
-        'make': request.args.get('make')
-    }
-
-    req = requests.get('http://www.fueleconomy.gov/ws/rest/vehicle/menu/model', params=vehicle, headers={'Accept': 'application/json'})
-    return jsonify(req.json()), req.status_code
-
-@app.route('/vehicle-menu-options')
-def vehicle_options():
-    vehicle = {
-        'year': request.args.get('year'),
-        'make': request.args.get('make'),
-        'model': request.args.get('model')
-    }
-
-    req = requests.get('http://www.fueleconomy.gov/ws/rest/vehicle/menu/options', params=vehicle, headers={'Accept': 'application/json'})
-    return jsonify(req.json()), req.status_code
-
-@app.route('/calc-trip-cost', methods=['POST'])
-def calc_trip_cost():
-    vehicle = json.loads(request.form['vehicle'])
-    directions = json.loads(request.form['trip'])
-    
-    #get request objects from fueleconomy.gov
-    reqVehicle = requests.get('http://www.fueleconomy.gov/ws/rest/vehicle/' + str(vehicle['vehicleId']), headers={'Accept': 'application/json'})
-    reqSharedMpg = requests.get('http://www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle/' + str(vehicle['vehicleId']), headers={'Accept': 'application/json'})
-    reqGasPrice = requests.get('http://www.fueleconomy.gov/ws/rest/fuelprices', headers={'Accept': 'application/json'})
-    
-    #get json from request objects
-    vehicleInfo = reqVehicle.json()
-
-    epaTripCost    = 0
-    sharedTripCost = 0
-
-    gasPriceInfo = reqGasPrice.json()
-    
-    #get combined mpg for fueltype 1
-    mpg = float(vehicleInfo['comb08U']) or float(vehicleInfo['comb08'])
-    
-    #determine fuel type to find gas price
-    fuelType = str(vehicleInfo['fuelType1']).lower().split(' ', 1)[0]
-
-    #use fueltype to find national average price for that type of fuel.
-    gasPrice = float(gasPriceInfo[fuelType])
-
-    #this is just placeholder. Don't know if there needs to be any setup to get this number.
-    distance = 0
-
-    for route in directions['routes']:
-        for leg in route['legs']:
-            distance += leg['distance']['value']
-
-    distance = distance * METERS_TO_MILES
-    print distance
-
-    if reqSharedMpg.content is not "":
-        sharedMpgInfo = reqSharedMpg.json()
-        
-        #get average user submitted mpg
-        sharedMpg = float(sharedMpgInfo['avgMpg'])
-
-        #calc tripcost using shared data
-        sharedTripCost = (distance / sharedMpg) * gasPrice
-    
-    #calc tripcost using epa estimate
-    epaTripCost = (distance / mpg) * gasPrice
-
-    tripCost = {}
-
-    if epaTripCost is not 0:
-        tripCost['epa'] = epaTripCost
-
-    if sharedTripCost is not 0:
-        tripCost['shared'] = sharedTripCost
-    
-    return jsonify(tripCost)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
